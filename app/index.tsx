38bc01b9055fc670f5dd6ceb47c585e8dcde979b
Gopi2923 <dgopidhikshan@gmail.com>
2024-10-01 13:10:49 +0530

    separated the styles file.

--- app/index.tsx
+++ app/index.tsx
@@ -7,6 +7,7 @@ import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 import ParameterList from '../app/Components/ParameterList';
 import qrimg1 from '../assets/images/qr-img-40.jpeg';
 import qrimg2 from '../assets/images/qr-img-99.jpeg';
+import { styles } from './HomePageStyles';
 
 const HomePage = () => {
   const [showPaymentModal, setShowPaymentModal] = useState(false);
@@ -234,113 +235,5 @@ const HomePage = () => {
     </ScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  headerContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-  logo: {
-    width: 100,
-    height: 100,
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    marginLeft: 20,
-  },
-  subtitle: {
-    fontSize: 24,
-    marginVertical: 10,
-  },
-  instantReportSection: {
-    marginVertical: 20,
-  },
-  instantReportButton: {
-    backgroundColor: '#007BFF',
-    padding: 10,
-    borderRadius: 5,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-    marginRight: 10,
-  },
-  clickIcon: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-  },
-  modalOverlay: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContent: {
-    backgroundColor: '#fff',
-    padding: 20,
-    borderRadius: 10,
-    width: '80%',
-    alignItems: 'center',
-  },
-  closeButton: {
-    alignSelf: 'flex-end',
-  },
-  closeText: {
-    fontSize: 24,
-    color: '#333',
-  },
-  modalTitle: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  paymentOptions: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-  },
-  optionButton: {
-    fontSize: 18,
-    padding: 10,
-    backgroundColor: '#007BFF',
-    color: '#fff',
-    borderRadius: 5,
-    textAlign: 'center',
-    margin: 10,
-    minWidth: '40%',
-  },
-  amountOptions: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-  },
-  qrCodeImage: {
-    width: 300,
-    height: 400,
-    marginVertical: 10,
-    borderRadius: 30,
-  },
-  confirmPaymentButton: {
-    backgroundColor: '#28a745',
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 20,
-  },
-  successModal: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default HomePage;
